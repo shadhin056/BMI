8cdd0af27da266ca15b3a6a5ba0a2aa28dbfa73e
Moniruzzaman ShadhiN <shadhinemail@gmail.com>
2020-07-17 22:14:52 +0600

    data pass

--- BMI Learn/ResultViewController.swift
+++ BMI Learn/ResultViewController.swift
@@ -9,9 +9,12 @@
 import UIKit
 
 class ResultViewController: UIViewController {
-
+    
+    @IBOutlet weak var bmiValue: UILabel!
+    var height: String!
     override func viewDidLoad() {
         super.viewDidLoad()
+        bmiValue.text=height
     }
 
 
--- BMI Learn/ViewController.swift
+++ BMI Learn/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var bmiValue: String?
     @IBOutlet weak var height: UITextField!
     
     @IBOutlet weak var txtView: UITextView!
@@ -21,9 +22,16 @@ class ViewController: UIViewController {
 
     @IBAction func calculate(_ sender: UIButton) {
         txtView.text =  height.text
+        bmiValue=height.text
     performSegue(withIdentifier: "goToResult", sender: self)
     }
-     
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         if segue.identifier == "goToResult" {
+             let destinationVC = segue.destination as! ResultViewController
+             destinationVC.height = bmiValue
+        
+         }
+     }
     
 }
 
